157e247fe291c60ef60c0d71a897ef79971ed60d
kmadz <karthikmprojects@gmail.com>
2025-05-19 12:16:16 -0500

    Added custom col order and bolding sig DEGs

--- R/quickHeatmap.R
+++ R/quickHeatmap.R
@@ -14,6 +14,7 @@
 #' @param output Character, path to output directory, default current directory
 #' @param padj Double, value for p-adjusted value threshold, default 0.05
 #' @param save Logical, whether to save to output directory or not, default TRUE
+#' @param colOrder Character vector, order for columns
 #'
 #' @importFrom pheatmap pheatmap
 #' @importFrom DESeq2 DESeq
@@ -33,6 +34,7 @@ quickHeatmap <- function(file = NULL,
                          info = 'condition',
                          clustRows = TRUE,
                          clustCols = TRUE,
+                         colOrder = NULL,
                          labelRows = TRUE,
                          numRows = 50,
                          num = "",
@@ -43,6 +45,11 @@ quickHeatmap <- function(file = NULL,
   if (num == "" | denom == "") {
     stop("Numerator and denominator for results not specified")
   }
+
+  # For future version: Add params onlySigs = FALSE, onlyNonSigs = FALSE
+  # if (onlySigs == TRUE & onlyNonSigs == TRUE) {
+  #   stop("Both onlySigs and onlyNonSigs are selected. Please only set one option to TRUE.")
+  # }
 
   input <- ddsHandler(file, ".", title = title)
   annot_info <- as.data.frame(colData(input$dds)[info])
@@ -84,28 +91,77 @@ quickHeatmap <- function(file = NULL,
 
   if (is.null(targets)) {
     targets <- filtered_res$gene_name[1:numRows]
+    sigs <- targets
     fontface_vector <- "plain"
   } else {
-    is_significant <- targets %in% filtered_res$gene_name
-    fontface_vector <- ifelse(is_significant, "bold", "plain")
+    sigs <- filtered_res$gene_name[filtered_res$gene_name %in% targets]
   }
 
-  numSigs <- nrow(filtered_res)
+  # save for later
+  # if (onlySigs) {
+  #   targets <- sigs
+  # }
+  #
+  # if (onlyNonSigs) {
+  #   targets <- targets[!filtered_res$gene_name %in% targets]
+  # }
 
+  numSigs <- nrow(filtered_res)
 
   selected_genes <- g2name %>%
     filter(gene_name %in% targets) %>%
     arrange(factor(gene_name, levels = targets)) %>%
     distinct(gene_name, .keep_all = TRUE)
 
-
   matching_genes <- intersect(selected_genes$gene_id, rownames(assay(vsd)))
   selected_counts <- assay(vsd)[match(matching_genes, rownames(assay(vsd))), ]
-
+  rownames(selected_counts) <- selected_genes$gene_name[match(matching_genes, selected_genes$gene_id)]
 
   # define z score function
   zscore <- t(scale(t(selected_counts)))
 
+  # determine how significant DEGs are and assign asterisks per level significance
+  get_asterisks <- function(gene_name) {
+    if (!gene_name %in% filtered_res$gene_name) return("")
+
+    padj <- filtered_res$padj[filtered_res$gene_name == gene_name]
+
+    if (padj < 0.001) return("***")
+    if (padj < 0.01) return("**")
+    if (padj < 0.05) return("*")
+    return("")
+  }
+
+  # apply boldface and asterisks to gene labels based on how significant they are
+  make_labels <- function(labels, targets) {
+    sapply(labels, function(x) {
+      asterisks <- get_asterisks(x)
+      if (x %in% targets) {
+        paste0("bold('", x, asterisks, "')")
+      } else {
+        paste0("plain('", x, asterisks, "')")
+      }
+    })
+  }
+
+  bold_labels <- make_labels(rownames(zscore), sigs)
+  plot_labels <- parse(text = bold_labels)
+
+
+  if (!is.null(colOrder)) {
+    # check if custom column order matches with the actual colnames
+    if (!all(colOrder %in% colnames(zscore))) {
+      stop(
+        paste(
+          "Some column names in column_order don't match the data.\nCorrect names are:\n",+paste(colnames(zscore), collapse =
+                                                                                                   ", ")
+        )
+      )
+    }
+    clustCols <- FALSE  # if custom order, override column clustering
+    zscore <- zscore[, colOrder, drop = FALSE]
+    annot_info <- annot_info[colOrder, , drop = FALSE]
+  }
 
   if (save) {
     path <- file.path(input$output, input$file_prefix)
@@ -129,19 +185,20 @@ quickHeatmap <- function(file = NULL,
     color = color,
     cluster_rows = clustRows,
     show_rownames = labelRows,
-    labels_row = selected_genes$gene_name,
-    fontface_row = fontface_vector,
+    #labels_row = selected_genes$gene_name,
+    labels_row = plot_labels,
+    #fontface_row = fontface_vector[match(rownames(zscore)[final$tree_row$order], selected_genes$gene_name)],
     cluster_cols = clustCols,
     border_color = NA,
     annotation_col = annot_info,
     treeheight_row = 0,
     treeheight_col = 0,
-    #annotation_colors = ann_colors,
     main = paste(input$title, "|", numSigs, "significant genes", sep = " "),  # Title of the heatmap
     silent = TRUE
   )
+
   print(final)
-  dev.off()
+  if (save) dev.off()
 
   return (final)
 }
